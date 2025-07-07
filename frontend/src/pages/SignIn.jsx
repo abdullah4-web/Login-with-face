5e7f639c8006c832797506aa83c8618b553fc87f
Abdullah Shah <112820581+abdullah4-web@users.noreply.github.com>
2025-07-07 16:26:35 +0400

    Update SignIn.jsx

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -63,7 +63,7 @@ const SignIn = () => {
       const imageFile = dataURLtoFile(imageSrc, 'face.jpg');
       formData.append('image', imageFile);
 
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch('http://127.0.0.1:5001/login', {
         method: 'POST',
         body: formData
       });
