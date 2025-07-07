1ba1e101675540477490e38cbc2c0e0d6930d937
Abdullah Shah <112820581+abdullah4-web@users.noreply.github.com>
2025-07-07 16:26:57 +0400

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -66,7 +66,7 @@ const SignUp = () => {
       const imageFile = dataURLtoFile(imageSrc, 'capture.jpg');
       formData.append('image', imageFile);
 
-      const response = await fetch('http://127.0.0.1:5000/register', {
+      const response = await fetch('http://127.0.0.1:5001/register', {
         method: 'POST',
         body: formData
       });
